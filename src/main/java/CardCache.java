5ba0915fda3479f4adbc3b05a485e1c762e86ef9
Gal Lerman <gal.lerman1@gmail.com>
2015-12-08 15:22:59 +0200

    Implemented the card cache in-memory with test

--- src/main/java/CardCache.java
+++ src/main/java/CardCache.java
@@ -8,6 +8,11 @@ import org.apache.http.impl.client.HttpClients;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,66 +21,46 @@ import java.util.Map;
  */
 public class CardCache {
 
-//  private static final String cardsApiUrl = "http://api.mtgapi.com/v2/cards";
-//  private static final String cardsApiUrl = "http://scry.me.uk/api.php?name=chronozoa";
-
-  private static final Map<String, Integer> cardNameToId = new HashMap<>(30 * 1000);
+  private static final Map<String, Integer> cardNameToId = new HashMap<String, Integer>(30 * 1000); //TODO: decide on whether the card name is unique or not
+  private static final CloseableHttpClient client = HttpClients.createDefault();
   private static boolean initialized = false;
 
-
-  public static Integer getIdFor(final String cardName) {
+  public static Integer getIdFor(final String cardName) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
     if (!initialized) {
       init();
     }
-
     return cardNameToId.get(cardName);
   }
 
-  private static void init(){
+  static Map<String, Integer> get() throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+    init();
+    return cardNameToId;
+  }
 
+  private static void init() throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+    Class.forName("com.mysql.jdbc.Driver").newInstance();
+    final Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/mtg?user=root&password=Qwer812$");
 
+    final Statement statement = conn.createStatement();
+    final ResultSet resultSet = statement.executeQuery("SELECT cd_id, cd_name FROM cd_card");
+
+    while(resultSet.next()) {
+      final int cardId = resultSet.getInt(1);
+      final String cardName = resultSet.getString(2);
+      cardNameToId.put(cardName, cardId);
+    }
     initialized = true;
   }
 
   public static String fetch(final String uri) throws IOException {
-//    HttpClient client = new HttpClient();
-//
-//    // Create a method instance.
-//    PostMethod method = new PostMethod(cardsApiUrl);
-//
-//    // Provide custom retry handler is necessary
-//    method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
-//            new DefaultHttpMethodRetryHandler(3, false));
-//
-//    try {
-//      // Execute the method.
-//      int statusCode = client.executeMethod(method);
-//
-//      if (statusCode != HttpStatus.SC_OK) {
-//        System.err.println("Method failed: " + method.getStatusLine());
-//      }
-//
-//      // Read the response body.
-//      final InputStream inputStream = method.getResponseBodyAsStream();
-//      StringWriter sw = new StringWriter();
-//      IOUtils.copy(inputStream, sw);
-//      return sw.toString();
-//    } finally {
-//      // Release the connection.
-//      method.releaseConnection();
-//    }
-////  }
-//    HttpGet httpget = new HttpGet(
-//            "http://api.mtgapi.com/v2/cards");
 
-    final CloseableHttpClient client = HttpClients.createDefault();
     HttpGet httpget = new HttpGet(uri);
     final CloseableHttpResponse response = client.execute(httpget);
     final HttpEntity entity = response.getEntity();
-    return inputStringToString(entity.getContent());
+    return inputStreamToString(entity.getContent());
   }
 
-  private static String inputStringToString(final InputStream is) throws IOException {
+  private static String inputStreamToString(final InputStream is) throws IOException {
       StringWriter sw = new StringWriter();
       IOUtils.copy(is, sw);
       return sw.toString();
